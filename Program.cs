50aa1db0e577d95522d251a74c450976f44c843f
petims-Rjr0987 <fetrarandriannirina@gmail.com>
2026-05-22 12:05:49 +0300

    Message contextuel explicite

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("notre premieur Jour pour csharp avec collaboration");
-Console.Write("Salut tout le monde");
+﻿string teamName ="Elite c# team";
+int members  =3;
+Console.WriteLine("");
+Console.WriteLine("Bienvenue dans notre programme");
+Console.WriteLine($"Nom de l'equpe {teamName} ");
+Console.WriteLine($"Nombre du membres {members}");
+
+
+/*git add .
+git commit -m "Fetra: Ajout des questions Nom et Pays"
+git push origin main*/
