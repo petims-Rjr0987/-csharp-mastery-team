1e2ed5c82717be7050b5bf21a54fca3daddcad06
Patrick <patrickmahatranga@gmail.com>
2026-05-22 06:56:12 +0200

    comprendre le variable les variable

--- Program.cs
+++ Program.cs
@@ -1,13 +1,27 @@
-<<<<<<< HEAD
+//<<<<<<< HEAD
 ﻿string teamName ="Elite C# team";
 int Members= 3;
+int Age=4;
 
 Console.WriteLine($"Welcome to the {teamName}!");
 Console.WriteLine("bien vennue dans notre programme");
 Console.WriteLine($"We have {Members} members in our team.");
+Console.WriteLine($"The average age of our members is {Age}.");
 
-=======
-﻿string teamName ="Elite c# team";
+string username="Fetra";git 
+int Age=21;
+double height=1.75;
+char grade='A';
+
+bool isStudents=true;
+Console.WriteLine("Username:"+username);
+console.writeLine("Age:"+Age);
+console.writeLine($"Height:{height}");    
+
+//console.WriteLine($"age de l'equpe {teamName} ");
+
+
+﻿/*string teamName ="Elite c# team";
 int members  =3;
 Console.WriteLine("");
 Console.WriteLine("Bienvenue dans notre programme");
@@ -17,5 +31,6 @@ Console.WriteLine($"Nombre du membres {members}");
 
 /*git add .
 git commit -m "Fetra: Ajout des questions Nom et Pays"
-git push origin main*/
+git push origin main
 >>>>>>> 50aa1db0e577d95522d251a74c450976f44c843f
+*/
