42f2f7f1856f70075e92e7b24abef8cff90a4755
petims-Rjr0987 <fetrarandriannirina@gmail.com>
2026-05-22 12:42:41 +0300

    Coprendre les variable

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+﻿/*<<<<<<< HEAD
 ﻿string teamName ="Elite C# team";
 int Members= 3;
 
@@ -6,7 +6,8 @@ Console.WriteLine($"Welcome to the {teamName}!");
 Console.WriteLine("bien vennue dans notre programme");
 Console.WriteLine($"We have {Members} members in our team.");
 
-=======
+*/
+/*=======*/
 ﻿string teamName ="Elite c# team";
 int members  =3;
 Console.WriteLine("");
@@ -14,8 +15,37 @@ Console.WriteLine("Bienvenue dans notre programme");
 Console.WriteLine($"Nom de l'equpe {teamName} ");
 Console.WriteLine($"Nombre du membres {members}");
 
+string username = "Alex";
+int age = 20;
+double height = 1.75;
+char grade = 'A';
+bool isStudent = true;
+
+Console.WriteLine(username);
+Console.WriteLine(age);
+Console.WriteLine(height);
+Console.WriteLine(grade);
+Console.WriteLine(isStudent);
+
+
+/*
+
+string username = "Alex";
+int age = 20;
+double height = 1.75;
+char grade = 'A';
+bool isStudent = true;
+
+Console.WriteLine(username);
+Console.WriteLine(age);
+Console.WriteLine(height);
+Console.WriteLine(grade);
+Console.WriteLine(isStudent);
+
+*/
+
 
 /*git add .
 git commit -m "Fetra: Ajout des questions Nom et Pays"
 git push origin main*/
->>>>>>> 50aa1db0e577d95522d251a74c450976f44c843f
+/*>>>>>>> 50aa1db0e577d95522d251a74c450976f44c843f*/
