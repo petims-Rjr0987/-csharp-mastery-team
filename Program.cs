7b62b6495095b1df7f6d2e5e741c3e4f63a5381f
Patrick <patrickmahatranga@gmail.com>
2026-05-26 11:08:16 +0200

    description

--- Program.cs
+++ Program.cs
@@ -23,33 +23,33 @@
   
   
   
-  string profession;
-  string objectif;
-  int nbprojectcreer;
+  string ?profession; 
+  string?objectif;
+  int ?nbprojectcreer;
   char reponse;
   char reponse2;
 
-  Console.WriteLine("Entrez votre profession :");
+  Console.Write("Entrez votre profession :");
     profession = Console.ReadLine();
     Console.WriteLine("Entrez votre objectif :");
     objectif = Console.ReadLine();
-    Console.WriteLine("Entrez le nombre de projets que vous avez créés :"); 
+    Console.Write("Entrez le nombre de projets que vous avez créés :"); 
     nbprojectcreer = int.Parse(Console.ReadLine()); 
     reponse = (nbprojectcreer > 5) ? 'O' : 'N';
     if (reponse == 'O')
     {
-        Console.WriteLine("Félicitations ! Vous avez créé plus de 5 projets.");
+        Console.Write("Félicitations ! Vous avez créé plus de 5 projets.");
         return;
     }
     else
     {
-        Console.WriteLine("N ! Vous avez créé moins de projets.");
-        return;
+        Console.Write("N ! Vous avez créé moins de projets.");
+        ;
     }
-    reponse2 = (profession == "Développeur" && objectif == "Maîtriser C#") ? 'O' : 'N';
+    reponse2 = (profession == "" && objectif == "") ? 'O' : 'N';
     if (reponse2 == 'O')    
     {
-        Console.WriteLine("Félicitations ! Vous êtes un développeur qui souhaite maîtriser C#.");
+        Console.Write("Félicitations ! Vous êtes un développeur qui souhaite maîtriser C#.");
         return;
     }
     else
