d1fdd3b5dfc645d258f7eddb0697b91024e6febf
petims-Rjr0987 <fetrarandriannirina@gmail.com>
2026-05-26 15:18:22 +0300

    terminal simple

--- Program.cs
+++ Program.cs
@@ -14,12 +14,40 @@ Console.WriteLine("");
 Console.WriteLine("Bienvenue dans notre programme");
 Console.WriteLine($"Nom de l'equpe {teamName} ");
 Console.WriteLine($"Nombre du membres {members}");
+/*mode de conception :
+Un professionnel pense :
 
+pourquoi int ?
+combien de mémoire ?
+et si l’âge dépasse la limite ?
+sécurité ?
+validation ?
+*/
 string username = "Alex";
+/*Validation : longuer , sécurité(vide ou nom), caractère ,
+
+pourqoui string: chainne de caratère, */
+
 int age = 20;
 double height = 1.75;
+/*pourqouio ?? : nombre(entier)
+
+nombre avec vurgil. deux chiffre après virgul
+sécurité??? : nombre vide(entrer vide )
+validation ?? sans virgul innacceptable 
+*/
 char grade = 'A';
 bool isStudent = true;
+/*
+Un professionnel pense :
+
+pourquoi int ?
+combien de mémoire ?
+et si l’âge dépasse la limite ?
+sécurité ?
+validation ?
+
+*/
 
 Console.WriteLine(username);
 Console.WriteLine(age);
@@ -49,3 +77,18 @@ Console.WriteLine(isStudent);
 git commit -m "Fetra: Ajout des questions Nom et Pays"
 git push origin main*/
 /*>>>>>>> 50aa1db0e577d95522d251a74c450976f44c843f*/
+
+/*day 2 : variable et entrés user */
+
+Console.Write("entrer votre nom :");
+name = Console.ReadLine();
+
+
+Console.Write("Votre age :");
+ageDay2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Votre origin:");
+city = Console.ReadLine();
+
+Console.Write("Votre competance proffisionelle :");
+profession = Console.ReadLine();
