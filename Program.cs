c2de5ffb961971c8e0ab179a7fd422c0f6b1e6c5
Patrick <patrickmahatranga@gmail.com>
2026-05-26 10:12:21 +0200

    fonction

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
   string profession;
   string objectif;
   int nbprojectcreer;
+  char reponse;
 
   Console.WriteLine("Entrez votre profession :");
     profession = Console.ReadLine();
@@ -9,4 +10,17 @@
     objectif = Console.ReadLine();
     Console.WriteLine("Entrez le nombre de projets que vous avez créés :"); 
     nbprojectcreer = int.Parse(Console.ReadLine()); 
+    reponse = (nbprojectcreer > 5) ? 'O' : 'N';
+    if (reponse == 'O')
+    {
+        Console.WriteLine("Félicitations ! Vous avez créé plus de 5 projets.");
+        return;
+    }
+    else
+    {
+        Console.WriteLine("N ! Vous avez créé moins de projets.");
+        return;
+    }
+    
+
     
