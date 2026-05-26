76da68b9c53556f2e493e382e209adb275fa489c
Patrick <patrickmahatranga@gmail.com>
2026-05-26 10:02:55 +0200

    origin

--- Program.cs
+++ Program.cs
@@ -1,66 +1,12 @@
-﻿/*<<<<<<< HEAD
-﻿string teamName ="Elite C# team";
-int Members= 3;
+﻿
+  string profession;
+  string objectif;
+  int nbprojectcreer;
 
-Console.WriteLine($"Welcome to the {teamName}!");
-Console.WriteLine("bien vennue dans notre programme");
-Console.WriteLine($"We have {Members} members in our team.");
-
-*/
-/*=======*/
-﻿string teamName ="Elite c# team";
-int members  =3;
-Console.WriteLine("");
-Console.WriteLine("Bienvenue dans notre programme");
-Console.WriteLine($"Nom de l'equpe {teamName} ");
-Console.WriteLine($"Nombre du membres {members}");
-
-string username = "Alex";
-int age = 20;
-double height = 1.75;
-char grade = 'A';
-bool isStudent = true;
-
-Console.WriteLine(username);
-Console.WriteLine(age);
-Console.WriteLine(height);
-Console.WriteLine(grade);
-Console.WriteLine(isStudent);
-
-
-/*
-
-string username = "Alex";
-int age = 20;
-double height = 1.75;
-char grade = 'A';
-bool isStudent = true;
-
-Console.WriteLine(username);
-Console.WriteLine(age);
-Console.WriteLine(height);
-Console.WriteLine(grade);
-Console.WriteLine(isStudent);
-
-*/
-
-
-/*git add .
-git commit -m "Fetra: Ajout des questions Nom et Pays"
-git push origin main*/
-/*>>>>>>> 50aa1db0e577d95522d251a74c450976f44c843f*/
-
-/*day 2 : variable et entrés user */
-
-Console.Write("entrer votre nom :");
-name = Console.ReadLine();
-
-
-Console.Write("Votre age :");
-ageDay2 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Votre origin:");
-city = Console.ReadLine();
-
-Console.Write("Votre competance proffisionelle :");
-profession = Console.ReadLine();
+  Console.WriteLine("Entrez votre profession :");
+    profession = Console.ReadLine();
+    Console.WriteLine("Entrez votre objectif :");
+    objectif = Console.ReadLine();
+    Console.WriteLine("Entrez le nombre de projets que vous avez créés :"); 
+    nbprojectcreer = int.Parse(Console.ReadLine()); 
+    
