e4239a087e950aed88b9af3f3a75c590c1b6c626
Patrick <patrickmahatranga@gmail.com>
2026-05-27 09:55:56 +0200

    fonction

--- Program.cs
+++ Program.cs
@@ -1,62 +1,60 @@
-﻿
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  string ?profession; 
+﻿string ?name;
+int ?age;
+string ?origin;
+ string ?profession; 
   string?objectif;
-  int ?nbprojectcreer;
+  int ?  nbprojectcreer;
   char reponse;
   char reponse2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   Console.Write("Entrez votre profession :");
     profession = Console.ReadLine();
     Console.WriteLine("Entrez votre objectif :");
     objectif = Console.ReadLine();
     Console.Write("Entrez le nombre de projets que vous avez créés :"); 
-    nbprojectcreer = int.Parse(Console.ReadLine()); 
+    nbprojectcreer = Convert.ToInt32(Console.ReadLine());   
     reponse = (nbprojectcreer > 5) ? 'O' : 'N';
     if (reponse == 'O')
     {
         Console.Write("Félicitations ! Vous avez créé plus de 5 projets.");
-        return;
+        ;
     }
     else
     {
         Console.Write("N ! Vous avez créé moins de projets.");
         ;
     }
-    reponse2 = (profession == "" && objectif == "") ? 'O' : 'N';
-    if (reponse2 == 'O')    
+    reponse2 = (profession == "" && objectif == "") ? '0' : 'n';
+    if (reponse2 == '0')    
     {
         Console.Write("Félicitations ! Vous êtes un développeur qui souhaite maîtriser C#.");
         return;
     }
     else
     {
-        Console.WriteLine("N ! Votre profession ou votre objectif ne correspond pas aux critères.");
+        Console.Write("N ! Vous n'avez pas rempli les champs de profession et d'objectif.");
         return;
-    }   
+    }
 
 
     
